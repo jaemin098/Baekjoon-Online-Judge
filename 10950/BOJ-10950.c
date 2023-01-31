9847c1fdee07dab9fc934e76b0316c64e304a025
jaemin098 <115396944+jaemin098@users.noreply.github.com>
2023-01-31 22:33:17 +0900

    Create BOJ-10950.c
    A+B - 3

---
+++ 10950/BOJ-10950.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  int a, b, tc;
+  
+  scanf("%d", &tc);
+  
+  for(int i = 0; i < tc; i++) {
+    scanf("%d %d", &a, &b);
+    printf("%d\n", a + b);
+  }
+  
+  return 0;
+}
