57ccfe81041079ba41f4c0527c0823d6fd963c59
jaemin098 <115396944+jaemin098@users.noreply.github.com>
2023-03-01 22:40:25 +0900

    Create BOJ-2743.c
    단어 길이 재기

---
+++ 2743/BOJ-2743.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char ahp[102];
+  int size;
+  
+  scanf("%s", ahp);
+  
+  for(int a = 0; ; a++) {
+    if(ahp[a] == '\0') {
+      break;
+    }
+  }
+  
+  size = strlen(ahp);
+  printf("%d\n", size);
+  
+  return 0;
+}
