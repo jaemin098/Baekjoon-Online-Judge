1f37f04bb4e36ac2d01223e43fdc41d7e9a18e00
jaemin098 <115396944+jaemin098@users.noreply.github.com>
2023-03-05 17:25:23 +0900

    Create BOJ-2675.c
    문자열 반복

---
+++ 2675/BOJ-2675.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  int t;
+  
+  scanf("%d", &t);
+  
+  for(int i = 0; i < t; i++) {
+    int r;
+    char s[21];
+    
+    scanf("%d %s", &r, s);
+    
+    for(int j = 0; j < strlen(s); j++) {
+      for(int k = 0; k < r; k++) {
+        printf("%c", s[j]);
+        }
+    }
+    
+    printf("\n");
+  }
+  
+  return 0;
+}
