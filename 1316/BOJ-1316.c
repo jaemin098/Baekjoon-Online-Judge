8ea50b670dafa68bfe0689f1f747d002372072da
jaemin098 <115396944+jaemin098@users.noreply.github.com>
2023-03-11 23:12:14 +0900

    Create BOJ-1316.c
    그룹 단어 체커

---
+++ 1316/BOJ-1316.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char input_words[101];
+  int n;
+  
+  scanf("%d", &n);
+  
+  int count = n;
+  
+  for(int i = 0; i < n; i++) {
+    char first = '0';
+    int Alphabet[26] = {0, };
+    scanf("%s", input_words);
+    
+    for(int j = 0; input_words[j] != '\0'; j++) {
+      if(first != input_words[j]) {
+        first = input_words[j];
+        Alphabet[input_words[j] - 'a'] += 1;
+      }
+      
+      if(Alphabet[input_words[j] - 'a'] == 2) {
+        count -= 1;
+        break;
+      }
+    }
+  }
+  
+  printf("%d", count);
+  
+  return 0;
+} 
