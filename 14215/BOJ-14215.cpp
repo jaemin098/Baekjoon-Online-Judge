024d2ff72b7a8e4f64a94c79061e4c6907ecd6e7
jaemin098 <115396944+jaemin098@users.noreply.github.com>
2023-04-13 10:09:00 +0900

    Create BOJ-14215.cpp
    세 막대

---
+++ 14215/BOJ-14215.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+  int arr[3] = {0, };
+  cin >> arr[0] >> arr[1] >> arr[2];
+  
+  sort(arr, arr + 3);
+  
+  if(arr[0] + arr[1] > arr[2]) {
+    cout << arr[0] + arr[1] + arr[2];
+  }
+  
+  else {
+    cout << (arr[0] + arr[1]) * 2 - 1;
+  }
+  
+  return 0;
+}
