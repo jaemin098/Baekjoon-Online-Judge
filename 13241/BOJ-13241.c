34073c7b4bb3ecbc1d49835c07b7f62aa47b71a1
jaemin098 <115396944+jaemin098@users.noreply.github.com>
2023-04-14 15:42:49 +0900

    Create BOJ-13241.c
    최소공배수

---
+++ 13241/BOJ-13241.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+long long gcd(long long a, long long b) {
+  if(b == 0) {
+    return a;
+  }
+  else {
+    return gcd(b, a % b);
+  }
+}
+
+int main() {
+  long long a, b;
+  scanf("%lld %lld", &a, &b);
+  
+  printf("%lld", a * b / gcd(a, b));
+  
+  return 0;
+}
