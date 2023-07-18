964d7d7b7d28fd4574e19169c8d20ebaab6af5ee
jaemin098 <115396944+jaemin098@users.noreply.github.com>
2023-07-18 23:06:50 +0900

    Create BOJ-14645.c
    와이버스 부릉부릉

---
+++ 14645/BOJ-14645.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int n, k;
+  scanf("%d %d", &n, &k);
+  
+  for(int i = 0; i < n; i++) {
+    int a, b;
+    scanf("%d %d", &a, &b);
+  }
+  
+  printf("비와이");
+  
+  return 0;
+}
