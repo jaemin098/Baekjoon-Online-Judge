793bfc060914a69664dbe8072fac9200d120463a
jaemin098 <115396944+jaemin098@users.noreply.github.com>
2023-09-22 22:39:04 +0900

    Create BOJ-26574.cpp
    Copier

---
+++ 26574/BOJ-26574.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  
+  while(n--) {
+    int num;
+    cin >> num;
+    
+    cout << num << ' ' << num << endl;
+  }
+  
+  return 0;
+}
