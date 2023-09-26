d23689156bbfa86817a1ed0af6bfeca6348923aa
jaemin098 <115396944+jaemin098@users.noreply.github.com>
2023-09-26 22:34:08 +0900

    Create BOJ-27324.cpp
    ゾロ目 (Same Numbers)

---
+++ 27324/BOJ-27324.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  
+  if(n / 10 == n % 10) {
+    cout << "1" << endl;
+  }
+  else {
+    cout << "0" << endl;
+  }
+  
+  return 0;
+}
