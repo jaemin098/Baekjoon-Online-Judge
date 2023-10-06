09d79c02fdf9f3d5b59ed3a1d3783f38d377e342
jaemin098 <115396944+jaemin098@users.noreply.github.com>
2023-10-06 13:06:39 +0900

    Create BOJ-15232.c
    Rectangles

---
+++ 15232/BOJ-15232.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int r, c;
+  scanf("%d\n%d", &r, &c);
+  
+  for(int i = 0; i < r; i++) {
+    for(int j = 0; j < c; j++) {
+      printf("*");
+    }
+    printf("\n");
+  }
+  
+  return 0;
+}
