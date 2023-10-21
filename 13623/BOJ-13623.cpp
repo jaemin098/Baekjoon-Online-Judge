caf35a53bc13c4a7ac591520a1ae1bd822b95fdc
jaemin098 <115396944+jaemin098@users.noreply.github.com>
2023-10-21 22:33:56 +0900

    Create BOJ-13623.cpp
    Zero or One

---
+++ 13623/BOJ-13623.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a, b, c;
+  cin >> a >> b >> c;
+  
+  if(a == b && b == c) {
+    cout << '*' << endl;
+  }
+  else if(a != b && a != c && b == c) {
+    cout << 'A' << endl;
+  }
+  else if(b != a && b != c && a == c) {
+    cout << 'B' << endl;
+  }
+  else if(c != a && c != b && a == b) {
+    cout << 'C' << endl;
+  }
+  
+  return 0;
+}
