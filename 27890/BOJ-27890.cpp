68cc6de3579448e898f87d41db8963d9224d7925
jaemin098 <115396944+jaemin098@users.noreply.github.com>
2023-12-11 22:35:23 +0900

    Create BOJ-27890.cpp
    특별한 작은 분수

---
+++ 27890/BOJ-27890.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int x, n;
+  cin >> x >> n;
+  
+  for(int i = 0; i < n; i++) {
+    if(x % 2 == 0) {
+      x = (x / 2) ^ 6;
+    }
+    else {
+      x = (2 * x) ^ 6;
+    }
+  }
+  
+  cout << x << endl;
+  
+  return 0;
+}
