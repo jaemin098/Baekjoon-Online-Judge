5b12bdb97322e32dd89298cb5f1d4c57d64c5d8c
jaemin098 <115396944+jaemin098@users.noreply.github.com>
2024-02-28 18:42:23 +0900

    Create BOJ-15128.cpp
    Congruent Numbers

---
+++ 15128/BOJ-15128.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  long long p1, q1, p2, q2;
+  cin >> p1 >> q1 >> p2 >> q2;
+  
+  if(p1 * p2 % (q1 * q2 * 2) == 0) {
+    cout << 1 << endl;
+  }
+  else {
+    cout << 0 << endl;
+  }
+  
+  return 0;
+}
