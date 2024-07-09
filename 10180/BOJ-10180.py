4ae8b40c03030ab0e9a282e6aecea481900df431
jaemin098 <115396944+jaemin098@users.noreply.github.com>
2024-07-09 20:13:25 +0900

    Create BOJ-10180.py
    Ship Selection

---
+++ 10180/BOJ-10180.py
@@ -0,0 +1,11 @@
+t = int(input())
+
+for _ in range(t):
+  n, d = map(int, input().split())
+  
+  cnt = 0
+  for _ in range(n):
+    v, f, c = map(int, input().split())
+    if d / v * c <= f: cnt += 1
+  
+  print(cnt)
